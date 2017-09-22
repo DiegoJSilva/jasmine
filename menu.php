ae9bb8847652bc58c15904f090373cc12f653b36
diego <diego@infoway-pi.com.br>
2017-09-22 14:54:21 -0300

    ajuste na section main

--- menu.php
+++ menu.php
@@ -28,7 +28,7 @@
 	  <span title="Main section">Main</span>
           <ul>
             <li>
-              <a href="main.php" title="Display the main page">Main page</a>
+              <a href="index.php?section=main" title="Display the main page">Main page</a>
             </li>
 	    <li>
 	      <a href="index.php?section=help" title="Get some help using JASmine">Help</a>
