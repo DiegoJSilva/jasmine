7c45a0033e13f5d8488d148f044352b86a000245
diego <diego@infoway-pi.com.br>
2017-09-26 09:25:17 -0300

    variável de print month

--- main.php
+++ main.php
@@ -6,16 +6,31 @@
   DB_connect($DB_host,$DB_login,$DB_pass);
   DB_select($DB_db);
 
-  $top10Users=jas_getUserRankingsMonth(15);
+  $top10Users=jas_getUserRankingsMonth(10);
   $top5Printers=jas_getPrinterRankings(5);
-  $top5Servers=jas_getServerRankings(5)
-
+  $top5Servers=jas_getServerRankings(5);
+  $month = array(
+    '01'=>'January',
+    '02'=>'February',
+    '03'=>'march',
+    '04'=>'April',
+    '05'=>'May',
+    '06'=>'June',
+    '07'=>'July',
+    '08'=>'August',
+    '09'=>'September',
+    '10'=>'October',
+    '11'=>'November',
+    '12'=>'December'
+  );
+  echo $month[date('m')];
 ?><!-- Begin Summary -->
-<h2>Summary of the Month</h2>
-<h3>Users Top10 Month</h3>
+
+<h2>Summary of the <?php echo $month[date('m')] ?></h2>
+<h3>Users Top10 <?php echo $month[date('m')] ?></h3>
 <?=($top10Users)?$top10Users:"<p>An error occured, please check the error messages.</p>"?>
-<h3>Printers Top5 Month</h3>
+<h3>Printers Top5 <?php echo $month[date('m')] ?></h3>
 <?=($top5Printers)?$top5Printers:"<p>An error occured, please check the error messages.</p>"?>
-<h3>Servers Top5 Month</h3>
+<h3>Servers Top5 <?php echo $month[date('m')] ?></h3>
 <?=($top5Servers)?$top5Servers:"<p>An error occured, please check the error messages.</p>"?>
 <!-- End Summary -->
